dae5b92b3080e18bd8225280fe2f767f806d83fb
japezhu <japezhu@tencent.com>
2026-04-23 17:47:40 +0800

    2026.4.23

--- HeroBuilder/Source/HeroBuilder/Private/Subsystems/HB_WaveSubsystem.cpp
+++ HeroBuilder/Source/HeroBuilder/Private/Subsystems/HB_WaveSubsystem.cpp
@@ -2,15 +2,144 @@
 
 
 #include "Subsystems/HB_WaveSubsystem.h"
+#include "Enemy/HB_Enemy_Base.h"
+
+DEFINE_LOG_CATEGORY(LogWaveSubsystem);
 
 void UHB_WaveSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
 	Super::Initialize(Collection);
-	UE_LOG(LogTemp, Log, TEXT("HB_WaveSubsystem Initialize"));
-	WaveData = LoadObject<UWaveData>(this, TEXT("/Game/Config/DA_WaveConfig.DA_WaveConfig"));
+    UE_LOG(LogWaveSubsystem, Log, TEXT("HB_WaveSubsystem Initialize"));
+    WaveData = LoadObject<UWaveData>(this, TEXT("/Game/Config/DA_WaveConfig"));
+    
+    if (!IsValid(WaveData))
+    {
+        UE_LOG(LogWaveSubsystem, Error, TEXT("Failed to load WaveData asset from /Game/Config/DA_WaveConfig"));
+        UE_LOG(LogWaveSubsystem, Error, TEXT("Please check if the asset exists in Content/Config/ folder"));
+    }
+    else
+    {
+        UE_LOG(LogWaveSubsystem, Log, TEXT("Successfully loaded WaveData asset"));
+    }
+}
+
+void UHB_WaveSubsystem::WaveTick(float DeltaTime)
+{
+	ENetMode NetMode = GetWorld()->GetNetMode();
+	if(NetMode != NM_DedicatedServer&&NetMode != NM_ListenServer&&NetMode != NM_Standalone)
+	{
+		return;
+	}
+	switch (WaveState)
+	{
+	case WS_Preparatory:
+	{
+		if (RemainingPreparatoryTime<0)
+		{
+			WaveState = WS_Fight;
+			UE_LOG(LogWaveSubsystem, Log, TEXT("Enter FightState"));
+		}
+		else
+		{
+			RemainingPreparatoryTime -= DeltaTime;
+		}
+	break;
+	}
+    case WS_Fight:
+	{
+		bool IsSpawnOver=true;
+		CurrentlyFightTime += DeltaTime;
+        for (int i=0;i< CurrentlyWaveEnemyConfigs.Num();i++  )
+		{
+			
+			FWaveEnemyConfig& EnemyConfig = CurrentlyWaveEnemyConfigs[i];
+			if (EnemyConfig.EnemyCount <= 0)
+			{
+				continue;
+			}
+			else
+			{
+				IsSpawnOver = false;
+			}
+			if (CurrentlyFightTime>=EnemyConfig.SpawnInterval)
+			{
+	
+				if(SpawnPoints.Num() != 0)
+				{
+					GetWorld()->SpawnActor<AHB_Enemy_Base>(EnemyConfig.EnemyClass, FTransform(FRotator(0, 0, 0), FVector(SpawnPoints[FMath::RandRange(0, SpawnPoints.Num() - 1)]), FVector(1, 1, 1)));
+
+				}
+				UE_LOG(LogWaveSubsystem, Log, TEXT("Spawn Enemy %s"), *EnemyConfig.EnemyClass->GetName());
+				EnemyConfig.EnemyCount -= 1;
+				EnemyConfig.SpawnInterval += CurrentlyWaveConfig.EnemyConfigs[i].SpawnInterval;
+			}
+		}
+		if (IsSpawnOver)
+		{
+				WaveState = WS_End;
+				UE_LOG(LogWaveSubsystem, Log, TEXT("Enter EndState"));
+		}
+		break;
+	}
+	case WS_End:
+		if (bAutoNextWave)
+		{
+			ActiveWaveByIndex(CurrentlyWaveIndex + 1, bAutoNextWave);
+			UE_LOG(LogWaveSubsystem, Log, TEXT("Enter PreparatoryState"));
+		}
+		else
+		{
+			WaveState = WS_Stop;
+			UE_LOG(LogWaveSubsystem, Log, TEXT("Enter StopState"));
+		}
+		break;
+	case WS_Stop:
+		break;
+	default:
+		break;
+	}
+	
 }
 
 void UHB_WaveSubsystem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	WaveTick(DeltaTime);
+}
+
+void UHB_WaveSubsystem::ActiveWaveByIndex(int32 InWaveIndex,bool AutoNextWave)
+{
+	if (!IsValid(WaveData))
+	{
+		UE_LOG(LogWaveSubsystem, Error, TEXT("WaveData Not Loaded"));
+		WaveState = WS_Stop;
+		return;
+	}
+	if (InWaveIndex >= WaveData->WaveConfigs.Num())
+	{
+		UE_LOG(LogWaveSubsystem, Error, TEXT("InWaveIndex is out of range"));
+		return;
+	}
+
+
+	CurrentlyWaveIndex = InWaveIndex;
+	CurrentlyWaveConfig = WaveData->GetWaveConfigByWaveIndex(InWaveIndex);
+	RemainingPreparatoryTime = CurrentlyWaveConfig.WaveInterval;
+	CurrentlyWaveEnemyConfigs = CurrentlyWaveConfig.EnemyConfigs;
+	CurrentlyFightTime = 0;
+	bAutoNextWave = AutoNextWave;
+	WaveState = WS_Preparatory;
+}
+
+void UHB_WaveSubsystem::SkipPreparatory()
+{
+	if (WaveState == WS_Preparatory)
+	{
+		WaveState = WS_Fight;
+	}
+	else
+	{
+		UE_LOG(LogWaveSubsystem, Error, TEXT("WaveState is not WS_Preparatory Cannot SkipPreparatory"));
+	}
+
 }
--- HeroBuilder/Source/HeroBuilder/Public/Subsystems/HB_WaveSubsystem.h
+++ HeroBuilder/Source/HeroBuilder/Public/Subsystems/HB_WaveSubsystem.h
@@ -7,7 +7,18 @@
 #include "Config/WaveData.h"
 #include "HB_WaveSubsystem.generated.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogWaveSubsystem, Log, All);
 class AHB_Enemy_Base;
+
+UENUM(BlueprintType)
+enum EWaveState:int8
+{
+	WS_Preparatory UMETA(DisplayName = "准备阶段"),
+	WS_Fight UMETA(DisplayName = "战斗阶段"),
+	WS_Stop UMETA(DisplayName = "停止阶段"),
+	WS_End UMETA(DisplayName = "结束阶段"),
+};
+
 
 /**
  * 
@@ -18,12 +29,25 @@ class HEROBUILDER_API UHB_WaveSubsystem : public UTickableWorldSubsystem
 	GENERATED_BODY()
 public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+protected:
+	void WaveTick(float DeltaTime);
+
 private:
 	TObjectPtr<UWaveData> WaveData;
-
+	TEnumAsByte<EWaveState> WaveState;
+	int32 CurrentlyWaveIndex;
+	float CurrentlyFightTime;
+	float RemainingPreparatoryTime;
+	TArray<FWaveEnemyConfig> CurrentlyWaveEnemyConfigs;
+	FWaveConfig CurrentlyWaveConfig;
+	TArray<FVector> SpawnPoints;
+	bool bAutoNextWave;
 public:
 
 	virtual void Tick(float DeltaTime) override;
 	virtual TStatId GetStatId() const override { return TStatId(); }
-	
+	UFUNCTION(BlueprintCallable)
+	void ActiveWaveByIndex(int32 Index,bool AutoNextWave);
+	UFUNCTION(BlueprintCallable)
+	void SkipPreparatory();
 };
